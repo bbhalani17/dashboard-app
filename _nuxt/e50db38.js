20226ca0db7044716a6049622aef3431103239b8
bbhalani <bhoomi.bhalani@factset.com>
2022-04-14 18:19:45 +0530

    9b1310d

---
+++ _nuxt/e50db38.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{381:function(n,e,t){"use strict";t.r(e);var l={name:"Index",components:{AppCards:t(375).default}},o=t(54),component=Object(o.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("app-cards")}),[],!1,null,"336172d4",null);e.default=component.exports;installComponents(component,{AppCards:t(375).default})}}]);
